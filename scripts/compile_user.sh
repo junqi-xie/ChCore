7ed2e7e88427ea2078b6627bebcbabcf6fa0665f
ieee-os-2021 <ipads@sjtu.edu.cn>
2021-12-13 10:00:00 +0800

    Lab 5: File System & Shell

--- scripts/compile_user.sh
+++ scripts/compile_user.sh
@@ -23,7 +23,12 @@ echo "building ramdisk ..."
 mkdir -p ramdisk
 echo "copy user/*.bin to ramdisk."
 cp process/*.bin ramdisk/
+cp shell/*.bin ramdisk/
+echo "copy user/*.srv to ramdisk."
+cp tmpfs/*.srv tmpfs/*.bin ramdisk/
 
 cd ramdisk
+echo "add fs_test files"
+cpio -idmv < ../../tmpfs/fs_test.cpio 2> /dev/null
 find . | cpio -o -Hnewc > ../ramdisk.cpio
 echo "succeed in building ramdisk."
